8db00d102672e3985001cc36bfe372a297c22337
PrimalPimmy <Prashant20.pm@gmail.com>
2023-03-07 17:25:34 +0530

    Risk scanning draft
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

---
+++ scan/scan.go
@@ -0,0 +1,89 @@
+// Package scan to scan for risks
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// podsWithPVC := &corev1.PodList{}
+
+	for _, pod := range pods.Items {
+		for _, volume := range pod.Spec.Volumes {
+			b, _ := json.MarshalIndent(volume.Projected, "", "    ")
+			fmt.Print(string(b))                  //returning correct output
+			fmt.Println(volume.Projected.Sources) //returning nil
+			//volume.Projected.Sources returns nil
+			// for _, v := range volume.Projected.Sources {
+			// 	if v.ServiceAccountToken != nil {
+			// 		fmt.Print(v.ServiceAccountToken)
+			// 	} else {
+			// 		fmt.Print("no")
+			// 	}
+			// }
+
+		}
+	}
+
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// Examples for error handling:
+	// - Use helper functions like e.g. errors.IsNotFound()
+	// // - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	// namespace := "wordpress-mysql"
+	// pod := "mysql-d8fb95554-p5c5m"
+	// _, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+	// if errors.IsNotFound(err) {
+	// 	fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	// 	fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+	// 		pod, namespace, statusError.ErrStatus.Message)
+	// } else if err != nil {
+	// 	panic(err.Error())
+	// } else {
+	// 	fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	// }
+
+}
